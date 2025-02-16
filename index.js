74f50b8c88febc354848df65af5933d1e58a787c
anujkumar025 <ffriction73@gmail.com>
2025-02-16 13:57:13 +0530

    fixed cors

--- index.js
+++ index.js
@@ -7,10 +7,23 @@ const PORT = process.env.PORT | 3000;
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://polls-backend.netlify.app", "https://fast-polls.vercel.app/"],
+    origin: [
+        "http://localhost:5173", 
+        "https://fast-polls.vercel.app"
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://fast-polls.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+
 app.use(express.json());
 
 app.post('/api/createpoll', async (req, res) => {
